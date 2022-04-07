b4f1bee2839fe936c9afdd88347a1201ca6e002c
Abreham Bekele <abresh273@gmail.com>
2022-04-07 10:57:35 +0000

    2nd try

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,22 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %lu. (unsigned long)sizeof(a)");
+        printf("Size of a int: %lu. (unsigned long)sizeof(b)");
+        printf("Size of a long int: %lu. (unsigned long)sizeo(c)");
+	printf("Size of a long long int: %lu. (unsigned long)sizeo(d)");
+        printf("Size of a float: %lu.(unsigned long)sizeo(e)");
+        return (0);
+}

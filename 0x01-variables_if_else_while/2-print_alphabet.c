519fa44a11af3feacec41aa71781e8492e135754
Abreham Bekele <abresh273@gmail.com>
2022-04-08 13:30:23 +0000

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,19 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
-	char c;
-	for(c='a';c <='z';c++)
-	   putchar(c);
-        putchar('\n');
-	return (0);
+char c;
+for(c='a';c <='z';c++)
+putchar("c");
+putchar('\n');
+return (0);
 }
+
 /* betty style doc for function main goes there */

2f9a313b097168ca98f1b66a55a47c1225fd43d1
Abreham Bekele <abresh273@gmail.com>
2022-04-08 09:09:40 +0000

    task 0-task3

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+nes (14 sloc)  195 Bytes
+   
+#include <stdio.h>
+
+/**
+ * main - Print all the letters except q and e
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char c;
+	for(c='a';c<='z';c++)
+	{
+		putchar("c");
+	}
+	for(c='A';c<='Z';c++)
+        {
+                putchar("c");
+        }
+	putchar('\n')
+
+	return (0);
+}
+

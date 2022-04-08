1a37de789539c30d8057af91413f9126d402a11e
Abreham Bekele <abresh273@gmail.com>
2022-04-08 12:58:35 +0000

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,10 +6,11 @@
 * 
 * Return: Always 0 (success/correct)
 */
-/* betty style doc for function main goes there */
+
 int main(void)
 {
 int n;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */

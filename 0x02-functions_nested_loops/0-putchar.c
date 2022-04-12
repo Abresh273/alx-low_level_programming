243e3c496af3c24710889031f94a39c2340950cc
Abreham Bekele <abresh273@gmail.com>
2022-04-12 10:17:43 +0000

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+
+{
+
+
+
+}

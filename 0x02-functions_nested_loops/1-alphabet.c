0cc6f48615b72f97638efb1076e4fc04f84de331
Abreham Bekele <abresh273@gmail.com>
2022-04-12 11:10:11 +0000

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_alphabet - print alphabet lowercase
+*
+* Return: Always 0.
+*/
+void print_alphabet(void)
+{
+	for(c = "a";c <= "z"; c++)
+	{
+		putchar("$c",c);
+	}
+	return 0;
+}

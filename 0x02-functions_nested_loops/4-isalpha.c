cb1d52a82c42bf6bfc37cd497cce26d1e803cfd1
Abreham Bekele <abresh273@gmail.com>
2022-04-12 12:17:42 +0000

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *int _isalpha: cheacks for aplphabetic charator
+ *@c: The int to print
+ *
+ * Retirn: Always 0.
+ *
+ */
+
+int _isalpha(int c)
+{
+if (c<="Z" && c>="A" || (c<="z" && c>="a")
+{
+return (1);
+}
+else 
+{
+return (0);			
+}
+}

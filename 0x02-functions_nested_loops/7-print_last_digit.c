939698c8af3fad410867cdfd09fc3bfd1b68a53c
Abreham Bekele <abresh273@gmail.com>
2022-04-12 13:38:15 +0000

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,15 +7,20 @@
  * Return: Always 0.
  */
 
-int print_last_digit(int c)
+int print_last_digit(int n)
+
 {
-a = c / 10;
-if (c > 0)
+int last_digit;
+if (n < 0)
 {
-return (aa);
+last_digit = (-1 * (n % 10));
+_putchar (last_digit + '0');
+return (last_digit);
 }
 else
 {
-return (-aa);
+last_digit = (n % 10);
+_putchar (last_digit + '0');
+return (last_digit);
 }
 }

0ba2558f104b89e8b10574efd351943e81df7772
Abreham Bekele <abresh273@gmail.com>
2022-04-12 11:34:01 +0000

    1

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - check the code.
+*
+* Return: Always 0.
+*/
+void print_alphabet_x10(void);
+{
+    char c;
+    int a;
+    for (c = 'a'; c <= "z"; c++)
+    {
+	    for (a = 0; c <= 9; c++)
+	    {
+		    _putchar(c);
+	    }
+	    _putchar("\n");
+    }
+}

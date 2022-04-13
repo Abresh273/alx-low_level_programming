1a6b8b2ccc822e2bddb0c755173d593dc1ed3688
Abresh273 <101420907+Abresh273@users.noreply.github.com>
2022-04-13 14:40:50 +0300

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,8 +10,9 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
-if (a >= b && b >= c)
+int largest;
+
+if (a >= b && a >= c)
 {
 largest = a;
 }
@@ -23,5 +24,7 @@ else
 {
 largest = c;
 }
+
 return (largest);
 }
+© 2022 GitHub, Inc.

d3bdb18ebf504550a1ab41f4b971dbf9f71148eb
Abreham <abresh273@gmail.com>
2022-04-13 07:26:03 +0000

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,15 +4,15 @@
  *                     starting with 0.
  * @n: The value of the times table to be printed.
  */
-void print_times_table(int n)
+int print_times_table(int n)
 {
 int num, mult, prod;
 if(n < 15 && n > 0)
 {
 	for(num=0;num<=n;num++)
 	{   
-		for(mul=0; mul <= n;mul++)
-			_putchar(mul*num);
+		for(mult = 0; mult <= n; mult++)
+			_putchar(mult*num);
 		        _putchar(", ");
 	}
 }

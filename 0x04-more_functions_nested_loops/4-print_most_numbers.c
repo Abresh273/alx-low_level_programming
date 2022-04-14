e9f27cc22d2bff0886f2c3df73f970ca0bf16b14
Abreham <abresh273@gmail.com>
2022-04-14 11:34:59 +0000

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/*
+*print_most_numbers - print numbers
+*
+*Return: Always 0
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+		for (i = 0;i < 0;i++)
+		{
+			if (i == 4 || i == 9)
+			{
+			}
+			else
+			{
+				_putchar(i);
+			}
+			
+		}
+		_putchar('\n');
+	
+}

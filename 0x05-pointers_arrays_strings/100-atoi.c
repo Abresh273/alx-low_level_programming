f009d3f36532e94a0842408a1890c363a94f53c5
Abreham <abresh273@gmail.com>
2022-04-18 11:51:49 +0000

    8

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+# include "main.h"
+
+
+/**
+ *
+ * _atoi -  a function that convert a string to an integer.
+ *
+ * @s : input argument char
+ *
+ * Return : Always 0
+ *
+ */
+
+int _atoi(char *s)
+{
+	int i;
+	for (i=0;s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+}

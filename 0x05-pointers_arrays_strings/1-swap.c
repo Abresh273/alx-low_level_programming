21ce68332af5508e9eaf8c71761c0b77373b6d9d
Abreham <abresh273@gmail.com>
2022-04-18 06:16:36 +0000

    0

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * swap_int - fuction
+ * 
+ * @*a : input value first
+ *
+ * @*b :input value second
+ *
+ * Return : Always 0
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int *c;
+	*c = *b;
+	*b = *a;
+	*a = *c
+
+
+}

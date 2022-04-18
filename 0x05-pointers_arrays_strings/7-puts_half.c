3746ddfe6f4899fe715aa2622cf8acfbf763765d
Abreham <abresh273@gmail.com>
2022-04-18 10:57:33 +0000

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,22 +1,37 @@
 #include "main.h"
 
 /**
- *
- * puts_half - a function that prints half of a string.
- *
- *
- * @str : string input
- *
- * Return : Always 0.
+ * puts_half - print reverse characters.
+ * @str: validate the character
+ * Return: Always 0.
  */
-
 void puts_half(char *str)
 {
-	int i;
+	int i = 0, n;
+
+
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
 		i++;
 	}
+	if (i % 2 == 1)
+	{
+		n = (i + 1) / 2;
+		while (str[n] != '\0')
+		{
+		_putchar(str[n]);
+		n++;
+		}
+	}
+	else
+	{
+		n = (i / 2);
+		while (str[n] != '\0')
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	_putchar('\n');
 }
 

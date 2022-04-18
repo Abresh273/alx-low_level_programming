c1e9766c34dc935a85cf285e0c16895b7660304b
Abreham <abresh273@gmail.com>
2022-04-18 12:44:04 +0000

    11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,11 +1,24 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 /**
  *
- *
+ *task 11
  *
  *
  */
 
+void main(void)
+{
+	int num;
+	srnad(time(0);
+	num = rand();
+	printf("%i\n",num);
+	return (0);
+
+
+}
 

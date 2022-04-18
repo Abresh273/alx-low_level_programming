863e5afcb9de88d390d6de5cb7a436c8df8dc8d9
Abreham <abresh273@gmail.com>
2022-04-18 12:29:13 +0000

    10 help

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,29 +2,31 @@
 
 
 /**
+ * _atoi - print reverse characters.
+ * @s: Array
  *
- * _atoi -  a function that convert a string to an integer.
- *
- * @s : input argument char
- *
- * Return : Always 0
- *
+ * Return: Always 0.
  */
-
 int _atoi(char *s)
 {
-	int i,b;
-	char *a;
-	
-	for (i=0;s[i] != '\0'; i++)
+	int i = 0, sign = 1;
+	unsigned int num = 0;
+
+	while (s[i] != '\0')
 	{
-		if (s[i] >= 48 && s[i] <= 57)
+		if ((s[i] < '0' || s[i] > '9') && (s[i] == '-'))
 		{
-			a[i]=s[i];
+			sign = sign * -1;
 		}
+		else if ((s[i] >= '0' && s[i] <= '9'))
+		{
+			num = num * 10 + (s[i] - '0');
+			if ((s[i + 1] < '0' || s[i + 1] > '9'))
+			{
+				break;
+			}
+		}
+		i++;
 	}
-
-	b = int(a);
-
-	return (b);
+	return (num * sign);
 }

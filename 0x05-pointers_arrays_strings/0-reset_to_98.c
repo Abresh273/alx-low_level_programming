0702e370f6ffae96536f8529c362b07189bcabc0
Abreham <abresh273@gmail.com>
2022-04-18 06:02:33 +0000

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+ *reset_to_98 -  takes a pointeras parameter and updates the value
+ * 
+ *@n: integer input
+ *
+ * Return : Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}

4be97db17d461abb9efe7f50cd24b906d0404ea2
Abreham <abresh273@gmail.com>
2022-04-18 11:19:45 +0000

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,10 +13,19 @@
 void print_array(int *a, int n)
 {
 	int i =0;
-	 printf("%d, ",a[i]);
+	
 
 	for (i=1;i< n ;i++)
 	{
-		printf(", %d",a[i]);
+		if(i<n-1)
+		{
+			printf("%d, ",a[i]);
+
+		}
+		else
+		{
+			 printf("%d",a[i]);
+	        }
+		
 	}
 }

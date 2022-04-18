937dc3c6f08631fab6a24d2fc99d259c2ce3e66d
Abreham <abresh273@gmail.com>
2022-04-18 08:59:22 +0000

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - a function that prints every other character of a strin
+ *
+ *@str : string input
+ *
+ *Return : Always 0.
+ */
+
+void puts2(char *str)
+{
+	int i, j;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for(j=0;j<=i;j+2)
+	{
+		_putchar(s[j]);
+	}
+}
+

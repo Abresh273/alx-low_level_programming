c10553e7d50c35a2206324ffe5793f3424259984
Abreham <abresh273@gmail.com>
2022-04-18 06:58:06 +0000

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ * _puts - printout the str
+ *
+ * @*str : input string
+ *
+ * Return : Always 0.
+ *
+ */
+
+
+void _puts(char *str)
+{
+     for(i=0; str[i] != '\0'; i++ )
+     {
+	     _putchar(str[i]);
+     }
+     _putchar('\n');
+}
+

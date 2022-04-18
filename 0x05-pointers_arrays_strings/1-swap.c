568933bbb277094fdda0111065112039ec77f0b8
Abreham <abresh273@gmail.com>
2022-04-18 06:20:49 +0000

    1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * swap_int - fuction
- * @*a : input value first
- * 
- * @*b :input value second
- *
- * Return : Always 0
- *
- */
+* swap_int - fuction
+* @*a : input value first
+* 
+* @*b :input value second
+*
+* Return : Always 0
+*
+*/
 void swap_int(int *a, int *b)
 {
 	int c;
+
 	c = *b;
 	*b = *a;
 	*a = c;

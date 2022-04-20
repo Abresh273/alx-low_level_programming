1a393b7050c9816930981374438ae0a9b5f43a1b
Abreham bekele <abresh273@gmail.com>
2022-04-20 12:10:01 +0000

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,32 @@
 #include "main.h"
+
 /**
+ * _strcat - unction that concatenates two strings.
  *
- *
- *
- *
+ * @dest : destination string charactor
+ * 
+ * @src : Source charactor
+ * 
+ * Return : Always 0.
  *
  */
+
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int j = 0;
+char b;
+while(dest[i] != '\0')
+{
+i++;
+}
+while(src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';	
+return (dest);
+}
+

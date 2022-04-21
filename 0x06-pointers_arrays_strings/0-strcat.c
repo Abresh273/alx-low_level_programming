5006492db01a020b4db15245ddbb10adc74cac30
Abreham bekele <abresh273@gmail.com>
2022-04-21 10:46:53 +0000

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,20 +13,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-int i = 0;
-int j = 0;
-char b;
-while(dest[i] != '\0')
-{
-i++;
+	int c, c2;
+	while (dest[c])
+	{c++};
+	for (c2 =0;src[c2];c2++)
+	{
+		dest[c++];
+	}
+	return (dest);
 }
-while(src[j] != '\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-dest[i] = '\0';	
-return (dest);
-}
-

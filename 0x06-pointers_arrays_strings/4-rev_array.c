06169193c717b352caa2a5528a30892f685a77b9
Abreham bekele <abresh273@gmail.com>
2022-04-21 10:17:27 +0000

    Task 0

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *
+ * reverse_array -  a function that reverses the content array
+ *
+ * @a : first integer
+ *
+ *
+ * @b : second integer
+ *
+ *
+ * Return : reversed 
+ *
+ */
+void reverse_array(int *a, int n)
+{
+
+}
+

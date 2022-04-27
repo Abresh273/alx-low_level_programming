b66a22b0a42fb4caadf6a0daa06cd2861c2ef29d
Abreham bekele <abresh273@gmail.com>
2022-04-27 12:46:18 +0000

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,19 +9,13 @@
  *Return : Always 0.
  */
 
-
 void _puts_recursion(char *s)
 {
-	int i = 0;
-        printf("%s",s);
-	i++;
-	if(s[i] != '\0')
+	if (*s == 0)
 	{
-		
-                _puts_recrusion(s[i]);
-		return (0);
+		_putchar('\n');
+		return;
 	}
-	
-
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
-

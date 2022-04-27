fac21fa173343d6eb5af5dcd00e8456d91fffbb9
Abreham bekele <abresh273@gmail.com>
2022-04-27 12:53:42 +0000

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * _print_rev_recursion -  function that prints a string in reverse.
+ *
+ *
+ * @s : string input
+ *
+ *
+ * Return : Always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+	return (0);
+
+}

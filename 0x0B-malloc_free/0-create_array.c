d570a9589f6ec5c99f3aad1f8c079352c8e16e8a
Abresh273 <101420907+Abresh273@users.noreply.github.com>
2022-05-03 16:29:00 +0300

    Update 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked -  creates an array of chars, and initializes it with a specific char.
+ * @size: unsigned int of amount of needed memory
+ *c: charachter input
+ * Return: void
+ */
+char *create_array(unsigned int size, char c)
+{
+
+int c[size]=""
+
+}

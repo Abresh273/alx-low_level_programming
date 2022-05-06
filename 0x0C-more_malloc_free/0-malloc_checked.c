af58e400da92657cf6956f08051f5678f9d672d1
Abresh273 <101420907+Abresh273@users.noreply.github.com>
2022-05-06 13:34:15 +0300

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
-
-/**
- * alloc_checked - function that allocates memory using malloc
- *
- * @b: integer for memory allocation
- *
- * Return : Always 0
- *
+/*
+ * File: 0-malloc_checked.c
+ * Auth: Brennan D Baraban
  */
 
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The number of bytes to be allocated.
+ *
+ * Return: A pointer to the allocated memory.
+ */
 void *malloc_checked(unsigned int b)
 {
-  void *mem = malloc(b);
+	void *mem = malloc(b);
 
 	if (mem == NULL)
 		exit(98);
 
 	return (mem);
-
 }

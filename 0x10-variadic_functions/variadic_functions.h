b0351ca46664328b9dd5705ad41108f58544995d
Abresh273 <101420907+Abresh273@users.noreply.github.com>
2022-05-12 14:55:01 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,25 +1,29 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef _VARIADIC_FUNCTIONS_H
+#define _VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
+#include <stdio.h>
 
-int _putchar(char c);
+/**
+* struct fmt - Struct format
+* @f: type format
+* @p: print function
+*/
+typedef struct fmt
+{
+	char *f;
+	void (*p)(va_list s);
+} my_fmt;
+
+
+/*prototypes*/
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_a_char(char *separator, va_list args);
-void print_a_integer(char *separator, va_list args);
-void print_a_float(char *separator, va_list args);
-void print_a_char_ptr(char *separator, va_list args);
+int _strlen_recursion(char *s);
+void print_c(va_list s);
+void print_i(va_list s);
+void print_f(va_list s);
+void print_s(va_list s);
 
-/**
- * struct format_types - Struct format_types
- * @identifier: The conversion specifier
- * @f: The function pointer
- */
-typedef struct format_types
-{
-	char *identifier;
-	void (*f)(char *separator, va_list args);
-} f_dt;
 #endif

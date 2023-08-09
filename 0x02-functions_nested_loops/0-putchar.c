a6feba287c358897adcf4c1d58820d64a26e8630
Abreham <abresh273@gmail.com>
2023-08-09 13:28:07 +0000

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-/*purpose : the function
- **
+/*
+ *purpose : the function
+ *
  *return 0: g
  **/
 
-
-
 int main(void)
 {
+/* using putchra*/
 printf("_putchar\n");
 return (0);
 }

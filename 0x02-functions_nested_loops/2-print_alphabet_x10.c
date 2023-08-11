b9d87d37657888b5582fa0167e605323bf5afaf3
abresh <abreh273@gmail.com>
2023-08-11 14:06:52 +0000

    task 1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/*
+ *
+ *
+ *
+ *
+ *
+ */
+
+void print_alphabet_x10(void){
+
+char a = 'a';
+for(i=0;i<=10;i++){
+
+for ( a= 'a'; a<='z';a++){
+_putchar(a);
+}
+}
+
+
+return;
+}
+
+
+
